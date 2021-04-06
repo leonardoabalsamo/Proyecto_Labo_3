55a113579d1d4184cd663e32274ca5b9bb4d9fa7
Leonardo <leonardodanielabalsamo@gmail.com>
2021-04-05 21:52:03 -0300

    mi primer commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Indice</title>
+</head>
+<body>
+
+	<h1>Indice de ejercicios de la materia LABORATORIO III</h1>
+
+	<ul>
+		<li><a href="./Css"> Css </a></li>
+		<li><a href="./especiales"> Especiales </a></li>
+		<li><a href="./Html"> HTML </a></li>
+		<li><a href="./Jscript"> Jscript </a></li>
+		<li><a href="./Otros"> Otros </a></li>
+		<li><a href="./Php"> Php </a></li>
+	</ul>
+
+</body>
+</html>
